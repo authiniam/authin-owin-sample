f881c1fc9596ec1a3cea39be4b78f5c10a3d59d8
Mohammad Hosein Mazinani <mhmazinani1994@gmail.com>
2020-08-26 15:13:55 +0430

    init

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthinOwinSample.Startup))]
+namespace AuthinOwinSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
